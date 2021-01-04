6606072b3d01ea9b287545d42344232283ac1975
CharlesLefort7 <charles.lefort@edu.ece.fr>
2021-01-04 22:37:05 +0100

    first update
    base du site

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Transparent Login Form with Blur Background</title>
+		<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="contact-form">
+		<img alt="" class="avatar" src="IMG_7970.jpg">
+		<h2>Coucou Emma</h2>
+		<form action="accueil.php" method="post">
+			<p>Pour accéder a ton calendrier</p><input placeholder="le nom du chien" type="password" name="mot_de_passe"> <input type="submit" value="Bisous">
+			
+		</form>
+	</div>
+</body>
+</html>
+
